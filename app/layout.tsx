5b573b0d07b3ad544698edda73128815dba6c6e4
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-12-18 11:54:44 +0000

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
-
+import { Metadata } from 'next';
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Roy Dashboard',
+    default: 'Roy Dashboard'
+  },
+  description: 'Roy is a powerful and intuitive admin dashboard built with Next.js and Ant Design.',
+  metadataBase: new URL('https://next-learn-dashboard.sh'),
+}
+
+
 export default function RootLayout({
   children,
 }: {
